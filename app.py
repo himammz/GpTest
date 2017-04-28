66d6158fe3729cd26ee89b5d4531a45ef80b4bb4
Ibrahim Mostafa <Ibrahim Mostafa>
2017-04-28 16:38:17 +0200

    FF

--- app.py
+++ app.py
@@ -33,9 +33,6 @@ def webhook():
 
 def makeWebhookResult(req):
     print(req)
-    print(req.text())
-    newdata = json.loads(req.text())
-    print(newdata)
     if req.get("result").get("action") == "myNameIsHanoma":
         str = getDB()
         return { "speech" : str,
